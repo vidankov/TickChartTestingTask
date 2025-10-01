da056bf8b3a4c1b4ff33960f578aa8c5d05d4f3c
vidankov <slavavinsent@gmail.com>
2025-10-01 10:10:38 +0300

    Реализация базового класса TickChart

--- TickChart/TickChart.axaml.cs
+++ TickChart/TickChart.axaml.cs
@@ -1,7 +1,52 @@
+using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using ScottPlot.Avalonia;
+using TickChartControl.Models;
 
 namespace UTS.AvalonaiUI.ComponentTask1.TickChartControl;
 
 public class TickChart : TemplatedControl
 {
+    private readonly List<TickData> _ticks = new();
+    private AvaPlot? _plot;
+
+    public static readonly StyledProperty<int> MaxVisibleTicksProperty =
+        AvaloniaProperty.Register<TickChart, int>(
+            nameof(MaxVisibleTicks), defaultValue: 500);
+
+    public static readonly StyledProperty<string> ChartThemeProperty =
+        AvaloniaProperty.Register<TickChart, string>(
+            nameof(ChartTheme), defaultValue: "Dark");
+
+    public int MaxVisibleTicks
+    {
+        get => GetValue(MaxVisibleTicksProperty);
+        set => SetValue(MaxVisibleTicksProperty, value);
+    }
+
+    public string ChartTheme
+    {
+        get => GetValue(ChartThemeProperty);
+        set => SetValue(ChartThemeProperty, value);
+    }
+
+    public TickChart()
+    {
+        this.TemplateApplied += OnTemplateApplied;
+    }
+
+    private void OnTemplateApplied(object? sender, TemplateAppliedEventArgs e)
+    {
+        _plot = e.NameScope.Find<AvaPlot>("PART_Plot");
+
+        if (_plot != null)
+        {
+            InitializePlot();
+        }
+    }
+
+    private void InitializePlot()
+    {
+    }
 }
